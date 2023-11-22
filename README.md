04c4ff221ad084dc1e5b1844299d206c99429cf0
Constantinos Giannacoulis <77970890+cgiannacoulis@users.noreply.github.com>
2023-11-22 12:31:35 +0200

    Sample project's initial structure.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# java-path-spring-we-lead-showcase
+## Code.Learn - Java Learning & Development Path, Spring
+
+This project hosts all source code exhibited during lab sessions related to **Spring** module.
+``
---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/SpringCoreApplication.java
@@ -0,0 +1,11 @@
+package gr.codelearn.spring.showcase.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreApplication.class, args);
+	}
+}
